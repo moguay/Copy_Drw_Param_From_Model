ed2778cd1d9332fdb033b44bd0da9fbb3d97826a
miaousse <jr@civetta.fr>
2016-02-15 09:52:10 +0100

    Update holes.js
    trouver comme faire un highlight besoin de trouver comment l injecter dans une fonction.

--- holes.js
+++ holes.js
@@ -9,7 +9,7 @@ function Main() {
 		var textNode = document.createTextNode(modelHoles.Count + " holes found in the model to process and Part is a PCB Material " + Model.GetParam("SE_MATERIAL_1").Value.StringValue);
 		UI.appendChild(textNode);
 		UI.appendChild(document.createElement("br"));
-
+		// var holeLink = document.createElement("a");
 		var holeInfos = document.createElement("div");
 		// holesInfos.className = "cat";
 		
@@ -37,20 +37,29 @@ function Main() {
 				holeInfos.className = "holeInfo "+ classType;
 				var holeInfo1 = document.createElement("span");
 				holeInfo1.className = "id";
-				holeInfo1.id = modelHoles.Item(i).Id;
+				
+				// holeInfo1.id = modelHoles.Item(i).Id;
+				// holeInfo1.id = modelHoles.Item(i).GetName();
 
 				var holeInfo2 = document.createElement("span");
 				holeInfo2.className = "creation";
 
 				var holeInfo3 = document.createElement("span");
+				
 				var holeInfo3Input = document.createElement("input");
 				holeInfo3Input.type= "text";
 
 				var holeInfo4 = document.createElement("span");
 				var holeInfo4input = document.createElement("input");
 				holeInfo4input.type= "button";
+				//cette ligne rentre en erreur pourquoi puisque ligne 41 marche 
+				// holeInfo4Input.id = modelHoles.Item(i).Id;
+
+
+				holeInfo4input.className = "button";
 				holeInfo4input.value = "highlight";
-				holeInfo4input.onclick = "alert(modelHoles.Item(i).Id);";
+				// holeInfo4input.onclick = highlightHole(this.id);
+				//function(this.id){pfcCreate ("MpfcSelect").CreateModelItemSelection(Model.GetItemById(pfcCreate("pfcModelItemType").ITEM_FEATURE, 293), null).Display();};
 
 				holeInfo1.appendChild(document.createTextNode( modelHoles.Item(i).Id ));
 				
@@ -71,12 +80,24 @@ function Main() {
 			holeInfos.appendChild(holeInfo4);
 			UI.appendChild(holeInfos);
 		}
-		oSession.GetModelWindow (Model).Repaint(); 
+		oSession.GetModelWindow(Model).Repaint(); 
+
+// pfcCreate ("MpfcSelect").CreateModelItemSelection(Model.GetItemById(pfcCreate("pfcModelItemType").ITEM_FEATURE, "293"), null).Display();
+pfcCreate ("MpfcSelect").CreateModelItemSelection(Model.GetItemById(pfcCreate("pfcModelItemType").ITEM_FEATURE, modelHoles.Item(0).Id), null).Display();
+// pfcCreate ("MpfcSelect").CreateModelItemSelection(Model.GetItemById(pfcCreate("pfcModelItemType").ITEM_FEATURE, modelHoles.Item(2).Id), null).Display();
+// pfcCreate ("MpfcSelect").CreateModelItemSelection(Model.GetItemById(pfcCreate("pfcModelItemType").ITEM_FEATURE, modelHoles.Item(5).Id), null).Display();
+// pfcCreate ("MpfcSelect").CreateModelItemSelection(Model.GetItemById(pfcCreate("pfcModelItemType").ITEM_FEATURE, modelHoles.Item(6).Id), null).Display();
+
 	} else {
-		var textNode = document.createTextNode("Seriously?");
+		var textNode = document.createTextNode("The model  you are trying to process is not a PCB.");
 		UI.appendChild(textNode);
 	}
 }
+
+// function highlightHole (holeID) {
+	// function(){pfcCreate ("MpfcSelect").CreateModelItemSelection(Model.GetItemById(pfcCreate("pfcModelItemType").ITEM_FEATURE, parseInt(holeID), null).Display();};
+// }
+
 function pfcIsWindows () {
 	if (navigator.appName.indexOf ("Microsoft") != -1) {
 		return true;
